377cc63a37abfdf32e25b7767d47fbe072e216a0
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-02-13 21:52:55 +0530

    Adding initial files

---
+++ src/components/User/User.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+export default function User() {
+  const {id} = useParams()
+  return (
+    <div className='bg-gray-600 text-white text-3xl p-4 items-center flex justify-center'>User:{id}</div>
+  )
+}
